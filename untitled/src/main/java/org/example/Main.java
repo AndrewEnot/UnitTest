e12ead5a2b2edb9a3fee9f92ea687da0432476b3
AndrewEnot <103563559+AndrewEnot@users.noreply.github.com>
2022-12-21 11:43:50 +0200

    Initial commit 1

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/*
+ * @author Oksiuta Andrii
+ * $DATE
+ * $TIME
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
